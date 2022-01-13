0470efb2ea388772031100326b851d25d88d1047
Julien Garcia <julien.garcia-lp@laposte.net>
2022-01-13 16:36:36 +0100

    Récupération du nom de la catégorie pour personnaliser le nom du fichier d'extraction.

--- one_category/one_category_run.py
+++ one_category/one_category_run.py
@@ -18,11 +18,14 @@ LABELS = ["product_page_url",
           "review_rating",
           "image_url"]
 
+
 # choose book category
-url_category_to_scrap = category_choice()
+choose_a_category = category_choice()
+url_category_to_scrap = choose_a_category[0]
+category_name = choose_a_category[1]
 
 # list of urls of the chosen category
-url_list_for_category = get_urls_book_category(url=url_category_to_scrap)
+url_list_for_category = get_urls_book_category(url_to_scrap=url_category_to_scrap)
 print(url_list_for_category)
 
 # extract data for each book, stock in a list
@@ -44,9 +47,12 @@ path_to_extract.mkdir(exist_ok=True)
 path_to_extract_one_category = path_to_extract / "extract_one_category"
 path_to_extract_one_category.mkdir(exist_ok=True)
 
+category_name = category_name.replace(" ", "_")
+extract_file = f"extract_{category_name}.csv"
+
 
 # save datas to csv file
-with open(file=f'{path_to_extract_one_category}/extract.csv', mode='w', encoding="utf-8", newline="") as f:
+with open(file=f'{path_to_extract_one_category}/{extract_file}', mode='w', encoding="utf-8", newline="") as f:
     writer = csv.DictWriter(f, fieldnames=LABELS)
     writer.writeheader()
     for elem in datas_list:
